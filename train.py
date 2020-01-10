1fc972dde2dadfc5b7bef859c3ef025c379db70e
Gil Nicula <vnicula@gmail.com>
2020-01-10 01:51:28 -0800

    add some metrics and todo

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import argparse
+import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pickle
@@ -8,6 +9,7 @@ import tensorflow as tf
 import time
 
 from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.models import load_model, Model
 from tensorflow.keras.layers import Dense, Input, GRU, Reshape, TimeDistributed
 
@@ -15,6 +17,24 @@ from tensorflow.keras.layers import Dense, Input, GRU, Reshape, TimeDistributed
 # https://github.com/tensorflow/tensorflow/issues/24520
 tf.enable_eager_execution()
 
+
+def rescale_list(input_list, size):
+    """Given a list and a size, return a rescaled/samples list. For example,
+    if we want a list of size 5 and we have a list of size 25, return a new
+    list of size five which is every 5th element of the origina list."""
+    assert len(input_list) >= size
+
+    # Get the number to skip between iterations.
+    skip = len(input_list) // size
+
+    # Build our new output.
+    output = [input_list[i] for i in range(0, len(input_list), skip)]
+
+    # Cut off the last one if needed.
+    return output[:size]
+
+
+# TODO investigate preprocessing of pixel values for pretrained MobileNet
 def read_file(file_path):
     
     names = []
@@ -28,7 +48,8 @@ def read_file(file_path):
                 names.append(key)
                 # labels.append(data[key][0])
                 labels.append(int(data[key][0] == 'FAKE'))
-                sample = np.array(data[key][1][:4])
+                sample = np.array(data[key][1][:8])
+                sample = preprocess_input(sample)
                 # sample = data[key][1][0]
                 samples.append(sample)
 
@@ -51,7 +72,7 @@ def create_model(input_shape, weights):
 
     input_layer = Input(shape=input_shape)
     # reshape = Reshape([224, 224, 3])(input_layer)
-    mobilenet = MobileNetV2(include_top=False, weights=weights, pooling='max')
+    mobilenet = MobileNetV2(include_top=False, weights=weights, pooling='avg')
     for layer in mobilenet.layers:
         layer.trainable = False
     # net = mobilenet(input_layer)
@@ -60,11 +81,25 @@ def create_model(input_shape, weights):
     out = Dense(1, activation='sigmoid')(net)
 
     model = Model(inputs=input_layer, outputs=out)
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.compile(loss='binary_crossentropy', optimizer='adam', 
+        metrics=['accuracy', #tf.keras.metrics.TruePositives(), tf.keras.metrics.TrueNegatives(),
+             tf.keras.metrics.FalsePositives(), tf.keras.metrics.FalseNegatives()])
     print(model.summary())
 
     return model
 
+
+def save_loss(H, N):
+    plt.figure()
+    plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+    # plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+    plt.plot(np.arange(0, N), H.history["acc"], label="train_acc")
+    # plt.plot(np.arange(0, N), H.history["val_acc"], label="val_acc")
+    plt.title("Training Loss and Accuracy")
+    plt.xlabel("Epoch #")
+    plt.ylabel("Loss/Accuracy")
+    plt.legend(loc="lower left")
+    plt.savefig("loss.jpg")
 
 
 if __name__ == '__main__':
@@ -83,13 +118,15 @@ if __name__ == '__main__':
     #     print(npelem[0], npelem[1])
     #     print(npelem[0].shape)
 
-    in_shape = (4, 224, 224, 3)
+    in_shape = (8, 224, 224, 3)
     # in_shape = (224, 224, 3)
     model = create_model(in_shape,
         'pretrained/mobilenet_v2_weights_tf_dim_ordering_tf_kernels_1.0_224_no_top.h5')
 
-    dataset = dataset.shuffle(buffer_size=256).batch(4)
-    history = model.fit(dataset, epochs=2)
+    dataset = dataset.shuffle(buffer_size=256).batch(8)
+    num_epochs = 2
+    history = model.fit(dataset, epochs=num_epochs)
+    save_loss(history, num_epochs)
 
     t1 = time.time()
     print("Execution took: {}".format(t1-t0))
