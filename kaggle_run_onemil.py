00dbce6b3850e64f2e9e067d5f823165919254da
Gil Nicula <vnicula@grailbio.com>
2020-02-26 14:32:04 -0800

    assume running kaggle run from source dir

--- kaggle_run_onemil.py
+++ kaggle_run_onemil.py
@@ -124,6 +124,69 @@ def detect_faces_bbox(detector, originals, images, batch_size, img_scale, face_s
     return faces
 
 
+class SeqWeightedAttention(keras.layers.Layer):
+    r"""Y = \text{softmax}(XW + b) X
+    See: https://arxiv.org/pdf/1708.00524.pdf
+    """
+
+    def __init__(self, use_bias=True, return_attention=False, **kwargs):
+        super(SeqWeightedAttention, self).__init__(**kwargs)
+        self.supports_masking = True
+        self.use_bias = use_bias
+        self.return_attention = return_attention
+        self.W, self.b = None, None
+
+    def get_config(self):
+        config = {
+            'use_bias': self.use_bias,
+            'return_attention': self.return_attention,
+        }
+        base_config = super(SeqWeightedAttention, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+    def build(self, input_shape):
+        self.W = self.add_weight(shape=(int(input_shape[2]), 1),
+                                 name='{}_W'.format(self.name),
+                                 initializer=keras.initializers.get('uniform'))
+        if self.use_bias:
+            self.b = self.add_weight(shape=(1,),
+                                     name='{}_b'.format(self.name),
+                                     initializer=keras.initializers.get('zeros'))
+        super(SeqWeightedAttention, self).build(input_shape)
+
+    def call(self, x, mask=None):
+        logits = K.dot(x, self.W)
+        if self.use_bias:
+            logits += self.b
+        x_shape = K.shape(x)
+        logits = K.reshape(logits, (x_shape[0], x_shape[1]))
+        ai = K.exp(logits - K.max(logits, axis=-1, keepdims=True))
+        if mask is not None:
+            mask = K.cast(mask, K.floatx())
+            ai = ai * mask
+        att_weights = ai / (K.sum(ai, axis=1, keepdims=True) + K.epsilon())
+        weighted_input = x * K.expand_dims(att_weights)
+        result = K.sum(weighted_input, axis=1)
+        if self.return_attention:
+            return [result, att_weights]
+        return result
+
+    def compute_output_shape(self, input_shape):
+        output_len = input_shape[2]
+        if self.return_attention:
+            return [(input_shape[0], output_len), (input_shape[0], input_shape[1])]
+        return input_shape[0], output_len
+
+    def compute_mask(self, _, input_mask=None):
+        if self.return_attention:
+            return [None, None]
+        return None
+
+    @staticmethod
+    def get_custom_objects():
+        return {'SeqWeightedAttention': SeqWeightedAttention}
+
+
 def extract_one_sample_bbox(video_path, max_detection_size, max_frame_count, face_size):
     start = time.time()
     imgs, imrs, img_scale = parse_vid(video_path, max_detection_size, max_frame_count)
@@ -150,7 +213,8 @@ def run(file_list):
 
     prediction_list = []
     # Note: Kaggle renames the model folder behind my back
-    model = load_model('/kaggle/input/featureextractormodel/one_model.h5', custom_objects=custom_objs)
+    # model = load_model('/kaggle/input/featureextractormodel/one_model.h5', custom_objects=custom_objs)
+    model = load_model('one_model.h5', custom_objects=custom_objs)
 #     print(model.summary())
 #     score_calibrator = joblib.load('/kaggle/input/featureextractormodel/score_calibration.pkl')
     len_file_list = len(file_list)
@@ -212,7 +276,7 @@ if __name__ == '__main__':
 
     t0 = time.time()
 
-    file_paths = glob.glob(os.path.join(input_dir, '../dset/test_videos/*.mp4'))
+    file_paths = glob.glob(os.path.join(input_dir, '/raid/scratch/tf_train/dset/test_videos/*.mp4'))
     test_files = [os.path.basename(x) for x in file_paths]
 
 #     try:
