c1813aea3b9e76f3a0d3f39079404a497cbdfb33
Ubuntu <ubuntu@ip-172-31-14-153.us-west-2.compute.internal>
2020-03-10 01:04:01 +0000

    fixes

--- train_and_tune.py
+++ train_and_tune.py
@@ -188,8 +188,9 @@ def compile_model(model, mode, lr):
     optimizer = tf.keras.optimizers.SGD(lr)
     if mode == 'train':
         if CMDLINE_ARGUMENTS.model_name == 'facenet' or CMDLINE_ARGUMENTS.model_name == 'vggface':
-            optimizer = tfa.optimizers.Lookahead(
-                optimizer = tf.keras.optimizers.RMSprop(lr, decay=1e-5, momentum=0.9))
+            # optimizer = tfa.optimizers.Lookahead(
+            #     optimizer = tf.keras.optimizers.RMSprop(lr, decay=1e-5, momentum=0.9))
+            optimizer = tf.keras.optimizers.RMSprop(lr, decay=1e-5, momentum=0.9)
         else:
             optimizer = tf.keras.optimizers.SGD(lr, momentum=0.9)
 
